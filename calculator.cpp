fabba4ec080eabec26c2d1ddd8d3736142216632
Moonquit <clash.of.clans64836@gmail.com>
2024-05-27 20:04:01 +0300

    Добавил функцию "Main" Она обработает запросы пользователя.

--- calculator.cpp
+++ calculator.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 
-/// Определите функции
+/// Визначте функції
 double add(double a, double b) {
     return a + b;
 }
 
-double minus(double a, double b) {
+double subtract(double a, double b) {
     return a - b;
 }
 
@@ -20,7 +20,46 @@ double divide(double a, double b) {
         return a / b;
     }
     else {
-        cout << "Error: Division by zero!" << endl;
+        cout << "Не можна ділити на нуль!" << endl;
         return 0;
     }
+}
+
+
+/// Обробка запитів користувача
+int main() {
+    setlocale(LC_ALL, "Ukrainian");
+    double num1, num2;
+    char operation;
+
+    cout << "Введіть перше число: ";
+    cin >> num1;
+
+    cout << "Введіть оператор (+, -, *, /): ";
+    cin >> operation;
+
+    cout << "Введіть друге число: ";
+    cin >> num2;
+
+    double result;
+    switch (operation) {
+    case '+':
+        result = add(num1, num2);
+        break;
+    case '-':
+        result = subtract(num1, num2);
+        break;
+    case '*':
+        result = multiply(num1, num2);
+        break;
+    case '/':
+        result = divide(num1, num2);
+        break;
+    default:
+        cout << "Недійсний оператор!" << endl;
+        return 1;
+    }
+
+    cout << "Результат: " << result << endl;
+    return 0;
 }
